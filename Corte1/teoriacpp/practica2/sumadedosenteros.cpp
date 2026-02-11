44a47323ecce205db6ff309a9af372179fab9eed
edwardpuma2006-sketch <edwardpuma2006@gmail.com>
2026-02-11 15:41:46 -0500

    Add files via upload

---
+++ Corte1/teoriacpp/practica2/sumadedosenteros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int  num1,num2,suma;
+
+cout << "ingresa primer  numero: ";
+cin >>num1;
+cout <<  "ingresa  segundo numero:  ";
+cin >>num2;
+
+
+suma =  num1 + num2;
+
+cout << "la suma" << num1 << "y"  << num2 << "es:"<< suma<< endl;
+return 0;
+}
