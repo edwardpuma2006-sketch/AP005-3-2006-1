df88936dd11f3c6fc8e7778dee763c644e42ca12
edwardpuma2006-sketch <edwardpuma2006@gmail.com>
2026-05-26 09:37:22 -0500

    Delete Corte2/punteros/01Puteros directory

--- Corte2/punteros/01Puteros/hola_desde_cpp.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> // Incluye la biblioteca de entrada y salida de C++.
-
-int main() { // Funcion principal: aqui comienza la ejecucion del programa.
-    std::cout << "Hola desde C++." << std::endl; // Muestra un mensaje en la consola.
-    return 0; // Informa que el programa finalizo correctamente.
-} // Fin de la funcion main.
